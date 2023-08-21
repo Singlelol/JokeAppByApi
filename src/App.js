3627258eb76ce2b009360d268026a6385b2ba5d9
Salov Ivan <singlelol21@gmail.com>
2023-08-22 00:23:24 +0300

    add: add function to load jokes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import JokeList from "./components/JokeList";
 import "./App.css";
@@ -23,7 +23,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null)
 
-  async function fetchJokesHandler() {
+  const fetchJokesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -37,7 +37,11 @@ function App() {
       setError(e.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchJokesHandler();
+  }, [fetchJokesHandler]);
 
   let content = <p>Шуток не найдено.</p>;
 
